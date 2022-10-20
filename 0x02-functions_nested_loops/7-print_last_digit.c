e5ddb2cda0629bc457fbcf8eb0c5c36ee6ff97c4
Davtinse <adetehinseadeyeye@gmail.com>
2022-10-20 10:39:59 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,10 @@ int print_last_digit(int n)
 {
 	int ld;
 
-	if (n < 0)
-		n = -n;
 	ld = n % 10;
-
+	if (ld < 0)
+		ld *= -1;
 	_putchar(ld + '0');
 
-	return (n % 10);
+	return (ld);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,22 +11,26 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		for (mult = 0; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			prod = num * mult;
-
-			if (prod <= 9)
+			if (mult == 0)
+				_putchar(prod + '0');
+			if (mult != 0 && prod < 10)
+			{
+				_putchar(',');
 				_putchar(' ');
-			else
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else if (prod >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((prod / 10) * '0');
-			_putchar((prod % 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
 }
-
