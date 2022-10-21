fdde38f8b9d511bd8b9645593dd321a00c8e2749
Davtinse <adetehinseadeyeye@gmail.com>
2022-10-20 20:47:19 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,10 +10,13 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
+
+
 	for (x = 0; x < n; x++)
 	{
-		for (y = 0; y < n; y++)
+		for (y = 0; y < x; y++)
 			_putchar(' ');
+
 		_putchar('\\');
 		_putchar('\n');
 	}
